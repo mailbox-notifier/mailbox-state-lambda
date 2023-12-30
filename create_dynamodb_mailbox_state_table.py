c891b9e6e563d0ea13c6d136509dd3aff8a044e9
gamename <tennis.n.smith@gmail.com>
2023-12-30 13:35:22 -0600

    Add db creator util

---
+++ create_dynamodb_mailbox_state_table.py
@@ -0,0 +1,102 @@
+"""
+This module provides functionality to interact with AWS DynamoDB. It includes
+capabilities to create a new DynamoDB table and initialize a key/value pair within it.
+Specifically, it creates a table named 'mailbox-state-key' and initializes a key 'open'
+with an unsigned integer value set to 0.
+
+Requirements:
+- AWS SDK for Python (Boto3)
+- Properly configured AWS credentials
+"""
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def create_dynamodb_table(table_name):
+    """
+    Creates a DynamoDB table with the specified table name.
+
+    The table is created with a single primary key attribute 'id' of type String.
+    It sets the provisioned read and write capacity units to 1.
+
+    Args:
+        table_name (str): The name of the table to create.
+
+    Prints the status of table creation or an error message if the table already exists
+    or if there is an exception during table creation.
+    """
+    dynamodb = boto3.client('dynamodb')
+
+    try:
+        response = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
+        print(f"Table creation initiated, status: {response['TableDescription']['TableStatus']}")
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceInUseException':
+            print(f"Table {table_name} already exists.")
+        else:
+            print(f"Error creating table: {e}")
+
+
+def create_initial_key_value(table_name, key_name, initial_value):
+    """
+    Creates an initial key/value pair in the specified DynamoDB table.
+
+    Args:
+        table_name (str): The name of the DynamoDB table to update.
+        key_name (str): The key of the item to create.
+        initial_value (int): The initial value to set for the key.
+
+    Adds a new item with the specified key and value to the table.
+    Prints a message indicating the action taken or an error if it occurs.
+    """
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    try:
+        response = table.put_item(
+            Item={
+                'id': key_name,
+                'value': initial_value
+            }
+        )
+        print(f"Key '{key_name}' created with initial value {initial_value}")
+    except ClientError as e:
+        print(f"Error putting item: {e}")
+
+
+def main():
+    """
+    Main function to execute table creation and item insertion.
+
+    It defines the table name and key/value pair details, then calls functions
+    to create the DynamoDB table and insert the initial key/value pair.
+    """
+    table_name = 'mailbox-state-key'
+    key_name = 'open'
+    initial_value = 0
+
+    create_dynamodb_table(table_name)
+    create_initial_key_value(table_name, key_name, initial_value)
+
+
+if __name__ == '__main__':
+    main()
