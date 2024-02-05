56d70bc3e5ebc5ea7692d71a85184b7836379b5c
gamename <tennis.n.smith@gmail.com>
2024-02-05 14:40:56 -0600

    update to 'path' vs 'rawPath'

--- lambda/mailbox_lambda.py
+++ lambda/mailbox_lambda.py
@@ -54,9 +54,9 @@ def handler(event, context):
     print(f"event:\n{event}")
     print(f"context:\n{context}")
 
-    if 'open' in event['rawPath']:
+    if 'open' in event['path']:
         mailbox_status = 'open'
-    elif 'closed' in event['rawPath']:
+    elif 'closed' in event['path']:
         mailbox_status = 'closed'
     else:
         print("Error: Invalid mailbox status.")
